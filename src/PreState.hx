5b669ef35236e5d299c1980605b2c1b18eec67fd
Tyler McCarthy <tweedlitweedli@gmail.com>
2021-03-24 20:57:51 -0500

    prestate in order to get rid of audio stutter

---
+++ src/PreState.hx
@@ -0,0 +1,37 @@
+package;
+
+import flixel.FlxG;
+import flixel.FlxState;
+import flixel.graphics.frames.FlxBitmapFont;
+import flixel.text.FlxBitmapText;
+import openfl.Assets;
+
+class PreState extends FlxState {
+    override public function create () {
+        super.create();
+
+        FlxG.mouse.visible = false;
+        FlxG.autoPause = false;
+
+        // var textBytes = Assets.getText(AssetPaths.miniset__fnt);
+        // var XMLData = Xml.parse(textBytes);
+        // var fontAngelCode = FlxBitmapFont.fromAngelCode(AssetPaths.miniset__png, XMLData);
+
+        // var text = new FlxBitmapText(fontAngelCode);
+        // text.letterSpacing = -1;
+        // text.setPosition(12, 12);
+        // text.text = 'We are WAITING to start_the_GAMe??!!!!';
+        // bgColor = 0xffaaaaaa;
+        // add(text);
+    }
+    
+    override public function update (elapsed:Float) {
+        if (FlxG.keys.justPressed.SPACE) {
+            startGame();
+        }
+    }
+
+    function startGame () {
+        FlxG.switchState(new PlayState());
+    }
+}
